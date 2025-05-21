228d72cd03ba82c7ac2a0f848c6605556c6c1d5c
elilourenco <lourencomonica318@gmail.com>
2025-05-21 09:23:08 +0100

    add the materil ui botton

--- my-redux/src/redux/slices/counterSlice.ts
+++ my-redux/src/redux/slices/counterSlice.ts
@@ -1,7 +1,12 @@
-// src/redux/slices/counterSlice.js
-import { createSlice } from '@reduxjs/toolkit';
+// src/redux/slices/counterSlice.ts
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../store'; // Import RootState for selector
 
-const initialState = {
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
   value: 0,
 };
 
@@ -15,12 +20,15 @@ export const counterSlice = createSlice({
     decrement: (state) => {
       state.value -= 1;
     },
-    incrementByAmount: (state, action) => {
+    incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
     },
   },
 });
 
 export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+// Selector to get the counter value from the state
+export const selectCount = (state: RootState) => state.counter.value;
 
 export default counterSlice.reducer;
--- my-redux/src/redux/store.ts
+++ my-redux/src/redux/store.ts
@@ -1,6 +1,8 @@
 // src/redux/store.ts
 import { configureStore } from '@reduxjs/toolkit';
 
+
+
 export const store = configureStore({
   reducer: {}, // We'll add reducers here later
 });
