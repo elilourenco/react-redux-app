4716ac4068533224e9630fd3ed685349b1e838e9
elilourenco <lourencomonica318@gmail.com>
2025-05-25 17:53:04 +0100

    add thenew pages

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -18,3 +18,20 @@ export default function Layout({children}:{children:React.ReactNode}){
     )
 
 }
+import { Box, Container } from "@mui/material";
+import React from "react";
+
+
+
+export default function Layout({children}:{children:React.ReactNode}){
+    return(
+        <>
+        <Box>
+            <Container maxWidth="lg"> {children}
+
+            </Container>
+        </Box>
+        </>
+    )
+
+}
