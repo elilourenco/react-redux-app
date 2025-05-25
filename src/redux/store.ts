2e70aa80560190a25b8b9bb38df0d9715bfea15c
elilourenco <lourencomonica318@gmail.com>
2025-05-25 12:37:37 +0100

    add the object

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,14 @@
 // src/redux/store.ts
 import { configureStore } from '@reduxjs/toolkit';
+import {counterSlice } from '../features/counter/counterSlice';
+import categoriesSlice from '../features/categories/categorySlice'
 
 
-
-export const store = configureStore({
-  reducer: {}, // We'll add reducers here later
+export const store=configureStore({
+  reducer:{
+    counter: counterSlice.reducer,
+    categories: categoriesSlice
+  }, // We'll add reducers here later
 });
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
