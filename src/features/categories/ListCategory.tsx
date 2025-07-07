7b8758980ce5cee08886acb85a9b670b7d43ace2
elilourenco <lourencomonica318@gmail.com>
2025-07-07 17:42:20 +0100

    add the apge

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -1,24 +1,25 @@
 import { Box,Typography } from "@mui/material"
-import { useSelector } from "react-redux";
-import { RootState } from "../../redux/store";
 import { selectCategories } from "./categorySlice";
+import { useAppSelector } from "../../app/hooks";
 
 
 
 function CategoryList(){
-    // If selectCategories returns { categories: Category[] }, extract the array:
-    const categoriesState = useSelector((state: RootState) => selectCategories(state));
+   
+    const categoriesState = useAppSelector(selectCategories);
+    const categories = categoriesState.categories;
  
-    const categories = categoriesState.categories ?? [];
-
+   
     return (
         <Box>
             <Typography variant="h3" component="h1">
                 CategoryList List
+            
+                {categories.map((category) => (
+                    <Typography key={category.id}>{category.name}</Typography>
+                ))}
+
             </Typography>
-            {categories.map((category) => (
-                <Typography key={category.id}>{category.name}</Typography>
-            ))}
         </Box>
     );
 }
