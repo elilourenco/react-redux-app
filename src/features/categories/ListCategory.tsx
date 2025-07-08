b95c96a0ef20c78dc321839c8326ebfe52a2e032
elilourenco <lourencomonica318@gmail.com>
2025-07-08 23:41:53 +0100

    add the page

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -2,7 +2,7 @@ import { Box,Button,Typography } from "@mui/material"
 import { selectCategories } from "./categorySlice";
 import { useAppSelector } from "../../app/hooks";
 import { Link } from "react-router-dom";
-import { DataGrid, GridColDef, GridRowsProp } from '@mui/x-data-grid';
+import { DataGrid, GridActionsColDef, GridColDef, GridRenderCellParams, GridRowsProp } from '@mui/x-data-grid';
 
 
 
@@ -12,17 +12,44 @@ function CategoryList(){
 
     const categories = categoriesState.categories.flat?.() ?? categoriesState.categories;
 
-    const rows: GridRowsProp = [
-  { id: 1, name: 'Data Grid', description: 'the Community version' },
-  { id: 2, name: 'Data Grid Pro', description: 'the Pro version' },
-  { id: 3, name: 'Data Grid Premium', description: 'the Premium version' },
-];
 
+    const rows: GridRowsProp = categories.map((category) =>({
+        id:category.id,
+        name:category.name,
+        description: category.description,
+        is_active: false,
+        createdAt: new Date(category.created_at).toLocaleDateString("en-US")
+    }));
+
+    
 
 const columns: GridColDef[] = [
-  { field: 'name', headerName: 'Product Name', width: 200 },
-  { field: 'description', headerName: 'Description', width: 300 },
+  { field: 'id', headerName: 'ID', flex:1 },
+  { field: 'name', headerName: 'Name', width: 300 },
+  {field:"description", headerName:"Description", flex:1},
+
+  {field:"isActive",
+     headerName:"Active ", 
+     flex:1, 
+     type: "boolean",
+    renderCell: renderIsActiveCell
+   },
+   {
+    field: "createdAt",
+    headerName: "Created At",
+    flex:1
+   }
 ];
+
+ function renderIsActiveCell(rowData:GridRenderCellParams){
+   
+    return(
+        <Typography color ={rowData.value ? "primary" :"secondary"}> 
+        {rowData.value ? "Active ": "Inactive"}
+        </Typography>
+    )
+}
+
 
 return (
     <Box maxWidth="lg" sx={{mt:4, mb:4}}>
@@ -47,7 +74,12 @@ return (
 
 
         <div style={{ height: 300, width: '100%' }}>
-            <DataGrid rows={rows} columns={columns} />
+            <DataGrid 
+
+              
+
+            rows={rows} 
+            columns={columns} />
         </div>
 
         
