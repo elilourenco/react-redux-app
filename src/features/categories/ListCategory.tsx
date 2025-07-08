5817da146805af629a6e8875853d30c3f6a07b6a
elilourenco <lourencomonica318@gmail.com>
2025-07-08 11:45:26 +0100

    ad the page

--- src/features/categories/ListCategory.tsx
+++ src/features/categories/ListCategory.tsx
@@ -7,21 +7,21 @@ import { useAppSelector } from "../../app/hooks";
 function CategoryList(){
    
     const categoriesState = useAppSelector(selectCategories);
-    const categories = categoriesState.categories;
- 
-   
-    return (
-        <Box>
-            <Typography variant="h3" component="h1">
-                CategoryList List
-            
-                {categories.map((category) => (
-                    <Typography key={category.id}>{category.name}</Typography>
-                ))}
 
-            </Typography>
-        </Box>
-    );
+    const categories = categoriesState.categories.flat?.() ?? categoriesState.categories;
+
+return (
+    <Box>
+        <Typography variant="h3" component="h1">
+            CategoryList List
+
+            {categories.map((category: any) => (
+                <Typography key={category.id}>{category.name}</Typography>
+            ))}
+
+        </Typography>
+    </Box>
+);
 }
 
 export default CategoryList;
