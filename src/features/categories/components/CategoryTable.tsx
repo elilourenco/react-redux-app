22c5497922218279b5873d9ef1b2a2670b8b892d
elilourenco <lourencomonica318@gmail.com>
2025-07-25 08:44:18 +0100

    add the page

--- src/features/categories/components/CategoryTable.tsx
+++ src/features/categories/components/CategoryTable.tsx
@@ -1,21 +1,25 @@
-import { GridFilterModel } from "@mui/x-data-grid";
+import {GridColDef, GridFilterModel, GridRenderCellParams } from "@mui/x-data-grid";
 import { Results } from "../../../types/Category"
+import { GridRenderEditCellParams } from "@mui/x-data-grid";
+import { IconButton } from "material-ui";
+import { Typography } from "@mui/material";
+import DeleteIcon from '@mui/icons-material/Delete';
+import { Link } from "react-router-dom";
 
- type Props ={
+type Props ={
     data: Results | undefined;
     perPage: number;
     isFetching: boolean;
     rowsPerPage?:number;
 
-
     handleOnPageChange:(page:number) => void;
     handleFilterChange:(filterModel:GridFilterModel) => void ;
     handleOnPageSizeChange:(pagesize: number) => void;
-    handleDeleteCategory:(id:number) => void;
+    handleDelete:(id:number) => void;
  };
  
- export function CategoriesTable({
 
+ export function CategoriesTable({
     data,
     perPage,
     isFetching,
@@ -25,6 +29,81 @@ import { Results } from "../../../types/Category"
     handleOnPageSizeChange,
     handleDelete,
 
+ } : Props) {
+    const componentProps ={
+        toolbar:{
+            showQuickFilter: true,
+            quickFilterProps:{debounceMs:500}
+        }
+    }
+
+const columns: GridColDef[] = [
+  
+  { field: 'name', headerName: 'Name', flex:1,
+    renderCell: renderNameCell,
+   },
+  
+  {field:"isActive",
+    headerName:"Active ", 
+    flex:1, 
+    type: "boolean",
+    renderCell: renderIsActiveCell
+   },
+   {
+    field: "createdAt",
+    headerName: "Created At",
+    flex:1
+   },
+
+   {
+    field:"id", headerName:"Actions", 
+    flex:1 ,
+    type: "string",
+    renderCell:renderActionsCell
+   }
+];
 
 
- } : Props) {}
+ }
+
+
+ function renderActionsCell(params:GridRenderEditCellParams){
+    return( 
+
+        <IconButton
+            color="secondary"
+            onClick={() => handleDelete(params.value)}
+            aria-label="delete"
+        >
+        <DeleteIcon />
+
+        </IconButton>
+    )
+}
+
+
+function renderNameCell(rowData: GridRenderCellParams) {
+    return(
+        <Link
+            style={{ textDecoration: "none" }}
+            to={`/categories/edit/${rowData.id}`}
+        >
+            <Typography>{rowData.value}</Typography>
+        </Link>
+    )
+}
+
+function renderIsActiveCell(rowData:GridRenderCellParams){
+   
+    return(
+        <Typography color ={rowData.value ? "primary" :"secondary"}> 
+        {rowData.value ? "Active ": "Inactive"}
+        </Typography>
+    )
+}
+
+
+
+
+
+export default CategoryList;
