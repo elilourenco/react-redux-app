14ac12c4539b2b531a0d7256ab6089178558b542
elilourenco <lourencomonica318@gmail.com>
2025-08-22 14:17:33 +0100

    add the page and test-utils with change and update the test

--- src/features/cast/CreateCastMembers.test.tsx
+++ src/features/cast/CreateCastMembers.test.tsx
@@ -2,14 +2,15 @@
 import { render } from "@testing-library/react";
 import { CreateCastMembers } from "./CreateCastMembers";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
 
 
 describe("CreateCastMember",()=>{
     it("Should  render correctly",()=>{
         const {asFragment} = render(
-            <BrowserRouter>
+            
                 <CreateCastMembers />
-            </BrowserRouter>
+            
         
     );
          expect( asFragment()).toMatchSnapshot()
--- src/utils/test-utils.tsx
+++ src/utils/test-utils.tsx
@@ -1,19 +1,24 @@
  import React, { PropsWithChildren } from 'react'
 import { render } from '@testing-library/react'
-import type { RenderOptions } from '@testing-library/react'
+import type {RenderOptions } from '@testing-library/react'
 import { configureStore } from '@reduxjs/toolkit'
 import { Provider } from 'react-redux'
 
-import type { AppStore, RootState } from '../app/store'
-import { setupStore } from '../app/store'
+import type {RootState } from '../app/store'
+
 // As a basic setup, import your same slice reducers
-import userReducer from '../features/cast/CastMembersSlice'
+
+import { apiSlice } from '../features/api/apiSlice'
+import { castMembersApiSlice } from '../features/cast/CastMembersSlice'
+import { SnackbarProvider } from 'notistack'
+import { BrowserRouter } from 'react-router-dom'
 
 // This type interface extends the default options for render from RTL, as well
 // as allows the user to specify other things such as initialState, store.
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   preloadedState?: Partial<RootState>
-  store?: AppStore
+  store?: ReturnType<typeof configureStore>
+ 
 }
 
 export function renderWithProviders(
@@ -21,14 +26,24 @@ export function renderWithProviders(
   extendedRenderOptions: ExtendedRenderOptions = {}
 ) {
   const {
-    preloadedState = {},
+  
     // Automatically create a store instance if no store was passed in
-    store = setupStore(preloadedState),
+    store = configureStore({
+      reducer:{
+        [castMembersApiSlice.reducerPath]: apiSlice.reducer,
+      },
+  }),
     ...renderOptions
   } = extendedRenderOptions
 
   const Wrapper = ({ children }: PropsWithChildren) => (
-    <Provider store={store}>{children}</Provider>
+    <Provider store={store}>
+      <BrowserRouter>
+      <SnackbarProvider>
+      {children}
+      </SnackbarProvider>
+      </BrowserRouter>
+      </Provider>
   )
 
   // Return an object with the store and all of RTL's query functions
