1dc928d44cb07315fdcfc034fd19c94e99de771c
elilourenco <lourencomonica318@gmail.com>
2025-08-29 13:30:07 +0100

    dd the page and commit with page

--- src/features/categories/CreateCategory.test.tsx
+++ src/features/categories/CreateCategory.test.tsx
@@ -23,4 +23,26 @@ describe("CreateCategory",()=>{
         expect(asFragment()).toMatchSnapshot();
   });
 
+
+  it("should create a new category", async ()=>{
+    renderWithProviders(<Createcategory/>);
+
+    const first_NameInput = screen.getByLabelText(/first_Name/i);
+    const last_NameInput = screen.getByLabelText(/last_Name/i);
+    const emailInput = screen.getByLabelText(/email/i);
+    const submitButton = screen.getByRole("button", { name: /submit/i });
+
+
+    fireEvent.change(first_NameInput, { target: { value: "John" } });
+    fireEvent.change(last_NameInput, { target: { value: "Doe" } });
+    fireEvent.change(emailInput, {target: { value: "doe@email.com"}})
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const successMessage = screen.getByText(/Category created successfully!/i);
+      expect(successMessage).toBeInTheDocument();
+    });
+
 });
+})
