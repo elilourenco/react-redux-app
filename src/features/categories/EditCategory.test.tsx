a9a44d25e9038c736b8737ffd955dd397faf8bb3
elilourenco <lourencomonica318@gmail.com>
2025-08-29 15:20:24 +0100

    add the test and error in  editcategory

--- src/features/categories/EditCategory.test.tsx
+++ src/features/categories/EditCategory.test.tsx
@@ -1,9 +1,10 @@
 
 import { setupServer } from "msw/lib/node";
-import { renderWithProviders, screen, waitFor } from "../../utils/test-utils";
+import { fireEvent, renderWithProviders, screen, waitFor } from "../../utils/test-utils";
 import CategoryEdit from "./EditCategory";
 import { http } from "msw";
-import { wait } from "@testing-library/user-event/dist/utils";
+import { baseUrl } from "../api/apiSlice";
+
 
 
 
@@ -17,6 +18,10 @@ const data = {
 const handlers = [
     http.get('/api/categories/:id', ({request, params, cookies}) => {
         return Response.json(data, { status: 200, headers: { "Content-Type": "application/json" }});
+    }),
+
+    http.put(`${baseUrl}/categories/1`, ({request, params, cookies}) => {
+        return Response.json({...data, ...request.json()}, { status: 200, headers: { "Content-Type": "application/json" }});
     })
 ]; 
 
@@ -45,5 +50,39 @@ describe('EditCategory', () => {
         expect(name).toHaveValue();
         
     })
+
+    fireEvent.change(name, { target: { value: "Jane" } });
+    fireEvent.change(email, { target: { value: "doe@email.com" } });
+
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+    fireEvent.click(submitButton);
+
+
+    await waitFor(() => { 
+        const text = screen.getByText(/Category updated successfully!/i);
+        expect(text).toBeInTheDocument(); })
   })
-});
+
+  it("should show error message on fetch failure", async () => {
+    server.use(
+        http.put('/api/categories/:id', ({request, params, cookies}) => {
+            return Response.json({ message: 'Failed to fetch category' }, { status: 500 });
+        })
+    );
+
+    renderWithProviders(<CategoryEdit />);
+    const name = screen.getByText(/John/i);
+    const email = screen.getByText(/doe@email.com/i);
+    
+    await waitFor(() => {
+        expect(name).toHaveValue();
+    })
+
+    fireEvent.change(name, { target: { value: "Jane" } });
+    fireEvent.change(email, { target: { value: "doe@email.com" } });
+
+    const submitButton = screen.getByRole('button', { name: /submit/i });
+    fireEvent.click(submitButton);
+})
+
+})
