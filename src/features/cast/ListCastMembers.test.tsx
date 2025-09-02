5929e4b35adf6cfd6f0c2d9b1f812e6e9bb17ea4
elilourenco <lourencomonica318@gmail.com>
2025-09-02 20:32:00 +0100

    add the page

--- src/features/cast/ListCastMembers.test.tsx
+++ src/features/cast/ListCastMembers.test.tsx
@@ -1,9 +1,24 @@
-import { renderWithProviders } from "../../utils/test-utils";
-import {htt} from "msw";
+import {http} from "msw";
+import {baseUrl} from "../api/apiSlice";
+import {setupServer} from "msw/node";
+import { fireEvent } from "@testing-library/react";
 import  {ListCastMembers} from "./ListCastMembers";
+import { Screen,waitFor,renderWithProviders } from "../../utils/test-utils";
+
+const handlers = [
+    http.get(`${baseUrl}/cast_members`,({request, params, cookies}) =>{
+      return new  Response(JSON.stringify({}), { status: 200, headers: { "Content-Type": "application/json" } });
+    }),
+]
+
+const server = setupServer(...handlers);
+
 
 
 describe('ListCastMembers', () => {
+    beforeAll(() => server.listen());
+    afterEach(() => server.resetHandlers());
+    afterAll(() => server.close());
   it('should render correctly', () => {
     // Test implementation goes here
 
