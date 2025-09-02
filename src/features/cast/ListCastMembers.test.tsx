0fc223fb029ee23dfde11c897726c7f42b38b5be
elilourenco <lourencomonica318@gmail.com>
2025-09-02 20:39:00 +0100

    add the page

--- src/features/cast/ListCastMembers.test.tsx
+++ src/features/cast/ListCastMembers.test.tsx
@@ -1,7 +1,7 @@
 import {http} from "msw";
 import {baseUrl} from "../api/apiSlice";
 import {setupServer} from "msw/node";
-import { fireEvent } from "@testing-library/react";
+import { fireEvent, screen } from "@testing-library/react";
 import  {ListCastMembers} from "./ListCastMembers";
 import { Screen,waitFor,renderWithProviders } from "../../utils/test-utils";
 
@@ -25,4 +25,18 @@ describe('ListCastMembers', () => {
     const  { asFragment } = renderWithProviders(<ListCastMembers/>);
     expect(asFragment()).toMatchSnapshot(); 
   });
+
+  it('should  render loading cast members', async () => {
+    renderWithProviders(<ListCastMembers/>);
+    
+    expect(screen.getByRole('progressbar')).toBeInTheDocument();   
+  });  
+  
+  it('should render sucess state', async () => {   
+    renderWithProviders(<ListCastMembers/>); 
+    await waitFor(() =>{
+        const table= screen.getByRole("table");
+        expect(table).toBeInTheDocument();
+    })    
+  });         
 });
